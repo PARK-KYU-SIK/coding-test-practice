e036fe4224549fcea56cbcce95c7869957083f8c
kyusik <parkksik11@gmail.com>
2022-08-11 23:07:09 +0900

    practice update 8/11-02

--- 00_Debuging_python.py
+++ 00_Debuging_python.py
@@ -1,52 +1,32 @@
-r1, c1, q1 = 6, 6, [[2,2,5,4],[3,3,6,6],[5,1,6,3]]            # [8, 10, 25]
-r2, c2, q2 = 3, 3, [[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]]  # [1, 1, 5, 3]
-r3, c3, q3 = 100, 97, [[1,1,100,97]]                             # [1]
+n1 = [1, 1, 1, 1, 1]    # 5
+n2 = [4, 1, 2, 1]       # 2
 
-rs = r1,r2,r3
-cs = c1,c2,c3
-qs = q1,q2,q3
+t1 = 3
+t2 = 4
+
+n = n1, n2
+t = t1, t2
 
 import numpy as np
 
-def make_ar(row, col) :
-    ar = np.arange(1,row * col +1).reshape(row,col)
-    return ar
-
-# def find_min(m1,m2,m3,m4) :
-#     minimum = min(min(m1), min(m2), min(m3), min(m4))
-#     return minimum
-
-def move_min(org_ar, new_ar,r1,c1,r2,c2) :
-    new_ar[r1-1,c1:c2] = org_ar[r1-1,c1-1:c2-1]     # row 1 행 우측 이동
-    # [1, 2:4] = [1, 1:3] 
-    m1 = new_ar[r1-1,c1:c2]
-    new_ar[r1:r2,c2-1] = org_ar[r1-1:r2-1,c2-1]     # col 2 열 아래 이동
-    # [2:5, 3] = [1:4, 3]
-    m2 = new_ar[r1:r2+1,c2-1]
-    new_ar[r2-1,c1-1:c2-1] = org_ar[r2-1,c1:c2]     # ro2 2 행 좌측 이동
-    # [4, 1:3] = [4, 2;4]
-    m3 = new_ar[r2-1,c1-1:c2-1]
-    new_ar[r1-1:r2-1,c1-1] = org_ar[r1:r2,c1-1]     # col 1 열 위로 이동
-    # [1:4, 1] = [2:4, 1]
-    m4 = new_ar[r1-1:r2,c1-1]
-    minimum = int(min(min(m1), min(m2), min(m3), min(m4)))
-    return new_ar, minimum
-
-def quiz_02(row, col, q) :
-    org_ar = make_ar(row,col)
-    new_ar = org_ar * 1
+def quiz_03(numbers,target) :
     answer = []
-    # cnt = 0
-    # print('org : {}\n'.format(cnt),org_ar)
-    for i in q :
-        # cnt += 1
-        row1,col1,row2,col2 = i
-        new_ar, minimum = move_min(org_ar,new_ar,row1,col1,row2,col2)
-        answer.append(minimum)
-        org_ar = new_ar * 1
-        # print('new : {}\n'.format(cnt),new_ar)
-    return answer
+    basic_ar = np.ones(shape=(1,int(len(numbers)))).tolist()[0]
+    cnt = 0
+    for times in range(-1,2,2) :
+        for i,x in enumerate(basic_ar) :
+            new_x = x * times
+            basic_ar[i] = new_x
+            check = int(sum(basic_ar))
+            if check == target :
+                cnt += 1
+                answer.append(basic_ar)
+            else :
+                pass
+    return cnt
 
-for input in zip(rs,cs,qs) :
-    row, col, q = input
-    print(quiz_02(row, col, q))
+quiz_03(n1,t1)
+
+# for input in zip(n,t) :
+#     num, tar = input
+#     print(quiz_03(num, tar))
